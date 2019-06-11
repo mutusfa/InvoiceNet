a4cbf6debc0d8a189b05350b3c970d7920f8561c
Julius Juodagalvis <julius.juodagalvis@gmail.com>
2019-06-11 14:30:11 +0100

    Package for inclusion in other projects.

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="invoice-net-mutusfa",
+    version="0.0.1",
+    author="Julius Juodagalvis",
+    author_email="juodagalvis@protonmail.com",
+    description="Models for invoice scanning",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mutusfa/InvoiceNet",
+    packages=setuptools.find_packages('.'),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
