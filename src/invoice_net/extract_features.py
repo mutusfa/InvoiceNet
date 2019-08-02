22fb3b7fd8b8dd0482634121b52f2d272c1ee5ad
Julius Juodagalvis <julius.juodagalvis@gmail.com>
2019-08-02 20:39:19 +0300

    Major refactoring.

--- src/invoice_net/extract_features.py
+++ src/invoice_net/extract_features.py
@@ -1,13 +1,3 @@
-import argparse
-import pickle
-import re
-import sys
-
-import datefinder
-from nltk import ngrams
-import pandas as pd
-from tqdm import tqdm
-
 """
 FEATURES:
 
@@ -15,14 +5,17 @@ raw_text:               The raw text
 
 processed_text:         The raw text of the last word in the N-gram
 
-text_pattern:           The raw text, after replacing uppercase characters with X, 
-                        lowercase with x, numbers with 0, repeating whitespace with 
-                        single whitespace and the rest with ?
+text_pattern:           The raw text, after replacing
+                            uppercase characters with X
+                            lowercase with x
+                            numbers with 0
+                            repeating whitespace with a single whitespace
+                            the rest with ?
 
-bottom_margin:          Vertical coordinate of the bottom margin of the 
+bottom_margin:          Vertical coordinate of the bottom margin of the
                         N-gram normalized to the page height
 
-top_margin:             Vertical coordinate of the top margin of the 
+top_margin:             Vertical coordinate of the top margin of the
                         N-gram normalized to the page height
 
 right_margin:           Horizontal coordinate of the right margin of the
@@ -35,7 +28,7 @@ has_digits:             Whether there are any digits 0-9 in the N-gram
 
 length:                 Number of characters in the N-gram
 
-position_on_line:       Count of words to the left of this N-gram normalized 
+position_on_line:       Count of words to the left of this N-gram normalized
                         to the count of total words on this line
 
 line_size:              The number of words on this line
@@ -50,6 +43,237 @@ parses_as_date:         Whether the N-gram parses as a date
 
 parses_as_number:       Whether the N-gram parses as an integer
 """
+
+import argparse
+import copy
+import logging
+import pickle
+import re
+import sys
+
+import datefinder
+from nltk import ngrams
+import pandas as pd
+from tqdm import tqdm
+
+
+EMPTY_SINGLE_GRAM = {
+    'raw_text': "",
+    'processed_text': "",
+    'text_pattern': "",
+    'length': 0,
+    'line_size': 0,
+    'position_on_line': 0,
+    'has_digits': False,
+    'bottom_margin': 1,
+    'top_margin': 0,
+    'left_margin': 0,
+    'right_margin': 1,
+    'page_width': sys.maxsize,
+    'page_height': sys.maxsize,
+    'parses_as_amount': False,
+    'parses_as_date': False,
+    'parses_as_number': False,
+    'label': 0,
+    'closest_ngrams': [-1, -1, -1, -1],  # left, top, right, bottom
+}
+
+
+def _calculate_distance_between_grams(first, second):
+    """Calculates distance vectors between two grams.
+
+    Direction is from from first to second.
+    """
+    above = first['top_margin'] - second['bottom_margin']
+    below = second['top_margin'] - first['bottom_margin']
+    right = second['left_margin'] - first['right_margin']
+    left = first['left_margin'] - second['right_margin']
+    left_margin_offset = abs(
+        second['left_margin'] - first['left_margin'])
+    return left, above, right, below, left_margin_offset
+
+
+def _parses_as_amount(text):
+    amount_pattern = r'(?:^|\s)\d+\.\d+(?:$|\s)'
+    try:
+        return re.search(amount_pattern, text)[0]
+    except TypeError:  # no matches
+        return None
+
+
+def _process_text(ngram):
+    """Returns proccessed text and what does it parse as."""
+    processed_text = []
+    as_date = False
+    as_amount = False
+    as_number = False
+    for word in ngram:
+        try:
+            as_date = bool(
+                list(datefinder.find_dates(word)))
+        except OverflowError:
+            as_date = False
+
+        word_is_number = word.isnumeric()
+        as_number = as_number or word_is_number
+
+        if _parses_as_amount(word):
+            processed_text.append('amount')
+            as_amount = True
+        elif as_date:
+            processed_text.append('date')
+        elif word_is_number:
+            processed_text.append('number')
+        else:
+            processed_text.append(word.lower())
+    as_number = as_number or as_date or as_amount
+    return ' '.join(processed_text), as_date, as_amount, as_number
+
+
+def _text_pattern(raw_text):
+    text = re.sub('\s+', ' ', raw_text)
+    text_pattern = ['?'] * len(text)
+    for i, char in enumerate(text):
+        if char.isnumeric():
+            text_pattern[i] = '0'
+        elif char.isspace():
+            text_pattern[i] = ' '
+        elif char.islower():  # works for unicode chars too
+            text_pattern[i] = 'x'
+        elif char.isupper():  # works for unicode chars too
+            text_pattern[i] = 'X'
+        # non-letter, non-number, npn-whitespace left as ?
+    return ''.join(text_pattern)
+
+
+def _group_by_file(df):
+    """Filters data into individual files
+
+    Also estimates:
+        width and height of each file.
+        x coordinate for each token in each line for every file.
+    """
+    files = {}
+    for _i, row in df.iterrows():
+        filename = row['files']
+        if filename not in files:
+            files[filename] = {
+                'lines': {'words': [], 'labels': [], 'ymin': [], 'ymax': []},
+                'xmin': sys.maxsize,
+                'ymin': sys.maxsize,
+                'xmax': 0,
+                'ymax': 0
+            }
+        tokens = row['words'].strip().split(' ')
+        char_length = (row['coords'][2] - row['coords']
+                       [0]) / len(row['words'].strip())
+        token_coords = [{'xmin': row['coords'][0],
+                         'xmax': row['coords'][0] + (char_length * len(tokens[0]))}]
+        for idx in range(1, len(tokens)):
+            token_coords.append({
+                'xmin': token_coords[-1]['xmax'] + char_length,
+                'xmax': token_coords[-1]['xmax'] + (char_length * (len(tokens[idx])+1))
+            })
+        files[filename]['lines']['words'].append(
+            {'tokens': tokens, 'coords': token_coords})
+        files[filename]['lines']['labels'].append(row['labels'])
+        files[filename]['lines']['ymin'].append(row['coords'][1])
+        files[filename]['lines']['ymax'].append(row['coords'][3])
+        files[filename]['xmin'] = min(
+            files[filename]['xmin'], row['coords'][0])
+        files[filename]['ymin'] = min(
+            files[filename]['ymin'], row['coords'][1])
+        files[filename]['xmax'] = max(
+            files[filename]['xmax'], row['coords'][2])
+        files[filename]['ymax'] = max(
+            files[filename]['ymax'], row['coords'][3])
+        files[filename]['page_width'] = \
+            files[filename]['xmax'] - files[filename]['xmin']
+        files[filename]['page_height'] = \
+            files[filename]['ymax'] - files[filename]['ymin']
+
+    return files
+
+
+def _fill_gram_features(
+    ngram,
+    tokens,
+    token_coords,
+    file_info,
+    ymin,
+    ymax,
+    label
+):
+    gram = copy.deepcopy(EMPTY_SINGLE_GRAM)
+    label_dict = {0: 0, 1: 1, 2: 2, 18: 3}
+    (
+        gram['processed_text'],
+        gram['parses_as_date'],
+        gram['parses_as_amount'],
+        gram['parses_as_number']
+    ) = _process_text(ngram)
+    raw_text = ' '.join(ngram)
+    gram['raw_text'] = raw_text
+    gram['text_pattern'] = _text_pattern(raw_text)
+    gram['length'] = len(' '.join(ngram))
+    gram['line_size'] = len(tokens)
+    gram['position_on_line'] = tokens.index(ngram[0])/len(tokens)
+    gram['has_digits'] = bool(re.search(r'\d', raw_text))
+    gram['left_margin'] = (
+        (token_coords[tokens.index(ngram[0])]['xmin'] - file_info['xmin']) /
+        file_info['page_width']
+    )
+    gram['top_margin'] = (ymin - file_info['ymin']) / file_info['page_height']
+    gram['right_margin'] = (
+        (token_coords[tokens.index(ngram[-1])]['xmax'] - file_info['xmin']) /
+        file_info['page_width']
+    )
+    gram['bottom_margin'] = \
+        (ymax - file_info['ymin']) / file_info['page_height']
+    gram['label'] = label
+    return gram
+
+
+def _find_closest_grams(grams, start=0):
+    """Finds closest ngrams to the left/right and top/down.
+
+    Modifies grams in-place.
+    """
+    for outer_gram in grams[start:]:
+        distance = {
+            'top': sys.maxsize,
+            'bottom': sys.maxsize,
+            'left': sys.maxsize,
+            'right': sys.maxsize,
+            'top_left': sys.maxsize,
+            'bottom_left': sys.maxsize,
+        }
+        for inner_gram_id, inner_gram in enumerate(grams[start:]):
+            inner_gram_id += start  # so id in loop matches id in sequence
+            if id(outer_gram) == id(inner_gram):
+                continue
+            left, above, right, below, left_margin_offset = \
+                _calculate_distance_between_grams(outer_gram, inner_gram)
+            # If in the same line, check for closest ngram to left and right
+            if above == below:
+                if distance['left'] > left > 0:
+                    distance['left'] = left
+                    outer_gram['closest_ngrams'][0] = inner_gram_id
+                if distance['right'] > right > 0:
+                    distance['right'] = right
+                    outer_gram['closest_ngrams'][2] = inner_gram_id
+            # If inner ngram is above outer gram
+            elif distance['top'] >= above >= 0 and \
+                    distance['top_left'] > left_margin_offset:
+                distance['top'] = above
+                distance['top_left'] = left_margin_offset
+                outer_gram['closest_ngrams'][1] = inner_gram_id
+            # If inner ngram is below outer gram
+            elif distance['bottom'] >= below >= 0 and \
+                    distance['bottom_left'] > left_margin_offset:
+                distance['bottom'] = below
+                distance['bottom_left'] = left_margin_offset
+                outer_gram['closest_ngrams'][3] = inner_gram_id
 
 
 def ngrammer(tokens, length=4):
@@ -74,150 +298,32 @@ def extract_features(path):
     with open(path, 'rb') as pklfile:
         df = pickle.load(pklfile)
 
-    files = {}
-
-    print("\nExtracting features...\n")
-
-    # Filters the data into individual files and finds out the minimum and maximum
-    # x and y coordinates to estimate the width and height of each file.
-    # Also estimates the x coordinate for each token in each line for every file.
-    for i, row in df.iterrows():
-        if row['files'] not in files:
-            files[row['files']] = {'lines': {'words': [], 'labels': [], 'ymin': [], 'ymax': []},
-                                   'xmin': sys.maxsize, 'ymin': sys.maxsize, 'xmax': 0, 'ymax': 0}
-        tokens = row['words'].strip().split(' ')
-        char_length = (row['coords'][2] - row['coords']
-                       [0]) / len(row['words'].strip())
-        token_coords = [{'xmin': row['coords'][0],
-                         'xmax': row['coords'][0] + (char_length * len(tokens[0]))}]
-        for idx in range(1, len(tokens)):
-            token_coords.append({'xmin': token_coords[-1]['xmax'] + char_length,
-                                 'xmax': token_coords[-1]['xmax'] + (char_length * (len(tokens[idx])+1))})
-        files[row['files']]['lines']['words'].append(
-            {'tokens': tokens, 'coords': token_coords})
-        files[row['files']]['lines']['labels'].append(row['labels'])
-        files[row['files']]['lines']['ymin'].append(row['coords'][1])
-        files[row['files']]['lines']['ymax'].append(row['coords'][3])
-        files[row['files']]['xmin'] = min(
-            files[row['files']]['xmin'], row['coords'][0])
-        files[row['files']]['ymin'] = min(
-            files[row['files']]['ymin'], row['coords'][1])
-        files[row['files']]['xmax'] = max(
-            files[row['files']]['xmax'], row['coords'][2])
-        files[row['files']]['ymax'] = max(
-            files[row['files']]['ymax'], row['coords'][3])
-
+    logging.info("\nExtracting features...\n")
+    files = _group_by_file(df)
     del df
 
-    grams = {'raw_text': [],
-             'processed_text': [],
-             'text_pattern': [],
-             'length': [],
-             'line_size': [],
-             'position_on_line': [],
-             'has_digits': [],
-             'bottom_margin': [],
-             'top_margin': [],
-             'left_margin': [],
-             'right_margin': [],
-             'page_width': [],
-             'page_height': [],
-             'parses_as_amount': [],
-             'parses_as_date': [],
-             'parses_as_number': [],
-             'label': [],
-             'closest_ngrams': []
-             }
-
-    label_dict = {0: 0, 1: 1, 2: 2, 18: 3}
-
+    grams = []
     # Calculates N-grams of lengths ranging from 1-4 for each line in each
     # file and calculates 17 features for each N-gram.
-    with tqdm(total=len(files)) as pbar:
-        for key, value in files.items():
-            num_ngrams = len(grams['raw_text'])
-            page_height = value['ymax'] - value['ymin']
-            page_width = value['xmax'] - value['xmin']
-            for i in range(len(value['lines']['words'])):
-                tokens = value['lines']['words'][i]['tokens']
-                token_coords = value['lines']['words'][i]['coords']
+    with tqdm(total=len(files)) as progress_bar:
+        for file_info in files.values():
+            old_num_grams = len(grams)
+            for line_num in range(len(file_info['lines']['words'])):
+                words = file_info['lines']['words'][line_num]
+                tokens = words['tokens']
+                token_coords = words['coords']
                 for ngram in ngrammer(tokens):
-                    grams['parses_as_date'].append(0.0)
-                    grams['parses_as_amount'].append(0.0)
-                    grams['parses_as_number'].append(0.0)
-                    processed_text = []
-                    for word in ngram:
-                        try:
-                            parses_as_date = bool(list(datefinder.find_dates(word)))
-                        except OverflowError:
-                            parses_as_date = False
-                        if parses_as_date:
-                            processed_text.append('date')
-                            grams['parses_as_date'][-1] = 1.0
-                        elif bool(re.search(r'\d\.\d', word)) or '$' in word:
-                            processed_text.append('amount')
-                            grams['parses_as_amount'][-1] = 1.0
-                        elif word.isnumeric():
-                            processed_text.append('number')
-                            grams['parses_as_number'][-1] = 1.0
-                        else:
-                            processed_text.append(word.lower())
-                    raw_text = ' '.join(ngram)
-                    grams['raw_text'].append(raw_text)
-                    grams['processed_text'].append(' '.join(processed_text))
-                    grams['text_pattern'].append(re.sub('[a-z]', 'x', re.sub('[A-Z]', 'X', re.sub('\d', '0', re.sub(
-                        '[^a-zA-Z\d\ ]', '?', raw_text)))))
-                    grams['length'].append(len(' '.join(ngram)))
-                    grams['line_size'].append(len(tokens))
-                    grams['position_on_line'].append(
-                        tokens.index(ngram[0])/len(tokens))
-                    grams['has_digits'].append(1.0 if bool(
-                        re.search(r'\d', raw_text)) else 0.0)
-                    grams['left_margin'].append(
-                        (token_coords[tokens.index(ngram[0])]['xmin'] - value['xmin']) / page_width)
-                    grams['top_margin'].append(
-                        (value['lines']['ymin'][i] - value['ymin']) / page_height)
-                    grams['right_margin'].append(
-                        (token_coords[tokens.index(ngram[-1])]['xmax'] - value['xmin']) / page_width)
-                    grams['bottom_margin'].append(
-                        (value['lines']['ymax'][i] - value['ymin']) / page_height)
-                    grams['page_width'].append(page_width)
-                    grams['page_height'].append(page_height)
-                    grams['label'].append(
-                        label_dict[value['lines']['labels'][i]])
-
-            # Finds the closest N-grams on all 4 sides for each N-gram
-            for i in range(num_ngrams, len(grams['raw_text'])):
-                grams['closest_ngrams'].append([-1] * 4)
-                distance = [sys.maxsize] * 6
-                for j in range(num_ngrams, len(grams['raw_text'])):
-                    d = [grams['top_margin'][i] - grams['bottom_margin'][j],
-                         grams['top_margin'][j] - grams['bottom_margin'][i],
-                         grams['left_margin'][i] - grams['right_margin'][j],
-                         grams['left_margin'][j] - grams['right_margin'][i],
-                         abs(grams['left_margin'][i] - grams['left_margin'][j])]
-                    if i == j:
-                        continue
-                    # If in the same line, check for closest ngram to left and right
-                    if d[0] == d[1]:
-                        if distance[2] > d[2] > 0:
-                            distance[2] = d[2]
-                            grams['closest_ngrams'][i][2] = j
-                        if distance[3] > d[3] > 0:
-                            distance[3] = d[3]
-                            grams['closest_ngrams'][i][3] = j
-                    # If this ngram is above current ngram
-                    elif distance[0] > d[0] >= 0 and distance[4] > d[4]:
-                        distance[0] = d[0]
-                        distance[4] = d[4]
-                        grams['closest_ngrams'][i][0] = j
-                    # If this ngram is below current ngram
-                    elif distance[1] > d[1] >= 0 and distance[5] > d[4]:
-                        distance[1] = d[1]
-                        distance[5] = d[4]
-                        grams['closest_ngrams'][i][1] = j
-
-            pbar.update(1)
+                    grams.append(_fill_gram_features(
+                        ngram,
+                        tokens,
+                        token_coords,
+                        file_info,
+                        file_info['lines']['ymin'][line_num],
+                        file_info['lines']['ymax'][line_num],
+                        file_info['lines']['labels'][line_num],
+                    ))
+            _find_closest_grams(grams, start=old_num_grams)
+            progress_bar.update(1)
 
     return pd.DataFrame(data=grams)
 
@@ -231,7 +337,7 @@ def main():
     args = ap.parse_args()
     features = extract_features(args.data)
     features.to_pickle(args.save_as, protocol=3)
-    print("\nSaved features as {}".format(args.save_as))
+    logging.info("\nSaved features as {}".format(args.save_as))
 
 
 if __name__ == '__main__':
