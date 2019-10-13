00b810c785e9a272ed40aa89f36d38d90f278314
Julius Juodagalvis <julius.juodagalvis@gmail.com>
2019-10-13 20:39:58 +0300

    Listing packages to install

--- setup.py
+++ setup.py
@@ -3,33 +3,31 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name="invoice-net-mutusfa",
-    version="0.0.1",
-    author="Julius Juodagalvis",
-    author_email="juodagalvis@protonmail.com",
-    description="Models for invoice scanning",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/mutusfa/InvoiceNet",
-    package_dir={'': 'src'},
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    install_requires=[
-        "datefinder",
-        "gensim",
-        "Keras",
-        "matplotlib",
-        "numpy",
-        "nltk",
-        "pandas",
-        "PyYAML",
-        "sklearn",
-        "tensorflow",
-        "tqdm",
-    ]
-
-)
+setuptools.setup(name="invoice-net-mutusfa",
+                 version="0.0.2",
+                 author="Julius Juodagalvis",
+                 author_email="juodagalvis@protonmail.com",
+                 description="Models for invoice scanning",
+                 long_description=long_description,
+                 long_description_content_type="text/markdown",
+                 url="https://github.com/mutusfa/InvoiceNet",
+                 package_dir={'': 'src'},
+                 packages=['invoice_net'],
+                 classifiers=[
+                     "Programming Language :: Python :: 3",
+                     "License :: OSI Approved :: MIT License",
+                     "Operating System :: OS Independent",
+                 ],
+                 install_requires=[
+                     "datefinder",
+                     "gensim",
+                     "Keras",
+                     "matplotlib",
+                     "numpy",
+                     "nltk",
+                     "pandas",
+                     "PyYAML",
+                     "sklearn",
+                     "tensorflow",
+                     "tqdm",
+                 ])
