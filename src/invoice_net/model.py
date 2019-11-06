086a2142f8841e6c75595908a43669597f759644
Julius Juodagalvis <julius.juodagalvis@gmail.com>
2019-11-07 01:00:11 +0200

    Split model initializiation/creation/compilation.

--- src/invoice_net/model.py
+++ src/invoice_net/model.py
@@ -1,5 +1,6 @@
 import os
 import pickle
+from typing import Any
 
 import numpy as np
 import tensorflow.keras.backend as K
@@ -28,7 +29,9 @@ def get_recall(predicted_labels, true_labels, target_label):
 
 def get_precision(predicted_labels, true_labels, target_label):
     mask = true_labels == target_label
-    return np.sum(predicted_labels[mask] == true_labels[mask]) / true_labels.size
+    return (
+        np.sum(predicted_labels[mask] == true_labels[mask]) / true_labels.size
+    )
 
 
 def get_f1_score(predicted_labels, true_labels, target_label):
@@ -44,6 +47,7 @@ def get_f1_scores(predictions, true_labels):
         f1_scores[target_label] = f1
     macrof1 = sum(f1_scores.values()) / len(true_labels)
     return f1_scores, macrof1
+
 
 def f1(y_true, y_pred, num_classes=6):
     def recall(y_true, y_pred):
@@ -71,6 +75,7 @@ def f1(y_true, y_pred, num_classes=6):
         predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
         precision = true_positives / (predicted_positives + K.epsilon())
         return precision
+
     if K.ndim(y_true) == K.ndim(y_pred):
         y_true = K.squeeze(y_true, -1)
     # convert dense predictions to labels
@@ -83,14 +88,17 @@ def f1(y_true, y_pred, num_classes=6):
         y_target_pred = K.cast(K.equal(y_pred_labels, target_label), K.floatx())
         prec = precision(y_target, y_target_pred)
         rec = recall(y_target, y_target_pred)
-        f1_score = 2*((prec*rec)/(prec+rec+K.epsilon()))
+        f1_score = 2 * ((prec * rec) / (prec + rec + K.epsilon()))
         macrof1 += f1_score / num_classes
     return macrof1
 
 
 class InvoiceNetInterface:
     def __init__(self, data_handler, config):
-        raise NotImplementedError("Model should be defined in __init__ method")
+        self.data_handler = data_handler
+        self.config = config
+        self.model = self.create_model(data_handler, config)
+        self.model.compile()
 
     def _create_needed_dirs(self):
         if not os.path.exists(self.config.log_dir):
@@ -105,30 +113,30 @@ class InvoiceNetInterface:
     @property
     def tensorboard_callback(self):
         return TensorBoard(
-            log_dir=self.config.log_dir, histogram_freq=1, write_graph=True)
+            log_dir=self.config.log_dir, histogram_freq=1, write_graph=True
+        )
 
     @property
-    def modelcheckpoints_callback(
-            self,
-            filename_format=".{epoch:02d}-{val_loss:.2f}-{val_acc:.2f}.hdf5",
-        ):
+    def modelcheckpoints_callback(self):
+        filename_format = ".{epoch:02d}-{val_loss:.2f}-{val_acc:.2f}.hdf5"
         return ModelCheckpoint(
             os.path.join(
                 self.config.checkpoint_dir,
                 self.__class__.__name__ + filename_format,
             ),
-            monitor='val_loss',
+            monitor="val_loss",
             verbose=0,
             save_best_only=True,
             save_weights_only=True,
-            mode='auto'
+            mode="auto",
         )
 
     def get_class_weights(self, true_labels):
         class_weights = compute_class_weight(
-                'balanced',
-                np.unique(true_labels),
-                self.data_handler.train_data['labels'])
+            "balanced",
+            np.unique(true_labels),
+            self.data_handler.train_data["labels"],
+        )
         return dict(enumerate(class_weights))
 
     def prepare_data(self, features, labels):
@@ -139,8 +147,7 @@ class InvoiceNetInterface:
         print("\nInitializing training...")
         self._create_needed_dirs()
         features, labels = self.prepare_data(
-            self.data_handler.features,
-            self.data_handler.labels
+            self.data_handler.features, self.data_handler.labels
         )
 
         self.model.fit(
@@ -149,58 +156,73 @@ class InvoiceNetInterface:
             batch_size=self.config.batch_size,
             verbose=True,
             epochs=self.config.num_epochs,
-            callbacks=[self.tensorboard_callback, self.modelcheckpoints_callback],
+            callbacks=[
+                self.tensorboard_callback,
+                self.modelcheckpoints_callback,
+            ],
             validation_split=0.125,
             shuffle=self.config.shuffle,
-            class_weight=self.get_class_weights(self.data_handler.labels)
+            class_weight=self.get_class_weights(self.data_handler.labels),
         )
 
-        self.model.save_weights(os.path.join(
-            self.config.model_path, self.__class__.__name__ + ".model"))
+        self.model.save_weights(
+            os.path.join(
+                self.config.model_path, self.__class__.__name__ + ".model"
+            )
+        )
+
+    def create_model(self, data_handler, config) -> Any:
+        raise NotImplementedError(
+            "Model should be defined in create_model method"
+        )
+
+    def compile_model(self):
+        self.model.compile(
+            loss="sparse_categorical_crossentropy",
+            optimizer="Adam",
+            metrics=["accuracy", f1],
+        )
 
 
 class InvoiceNet(InvoiceNetInterface):
-
-    def __init__(self, data_handler, config):
+    def create_model(self, data_handler, config):
         coordinates = Input(
-            shape=(data_handler.train_data['coordinates'].shape[1],),
-            dtype='float32', name='coordinates'
+            shape=(data_handler.train_data["coordinates"].shape[1],),
+            dtype="float32",
+            name="coordinates",
         )
-        aux_features = Input(shape=(
-            data_handler.train_data['aux_features'].shape[1],),
-            dtype='float32', name='aux_features'
+        aux_features = Input(
+            shape=(data_handler.train_data["aux_features"].shape[1],),
+            dtype="float32",
+            name="aux_features",
         )
-        words_input = Input(shape=(data_handler.max_length,),
-                            dtype='int32', name='words_input'
+        words_input = Input(
+            shape=(data_handler.max_length,), dtype="int32", name="words_input"
         )
         words = Embedding(
-            data_handler.embeddings.shape[0], data_handler.embeddings.shape[1],
+            data_handler.embeddings.shape[0],
+            data_handler.embeddings.shape[1],
             weights=[data_handler.embeddings],
-            trainable=False
+            trainable=False,
         )(words_input)
         output = Dropout(0.5)(words)
         output = GRU(
             config.num_hidden,
             dropout=0.5,
             recurrent_dropout=0.5,
-            go_backwards=True
+            go_backwards=True,
         )(output)
         output = concatenate([output, coordinates, aux_features])
-        output = Dense(config.num_hidden, activation='relu')(output)
-        output = Dense(config.num_hidden, activation='relu')(output)
+        output = Dense(config.num_hidden, activation="relu")(output)
+        output = Dense(config.num_hidden, activation="relu")(output)
         output = Dropout(0.5)(output)
-        output = Dense(config.num_hidden, activation='relu')(output)
-        output = Dense(data_handler.num_classes, activation='softmax')(output)
+        output = Dense(config.num_hidden, activation="relu")(output)
+        output = Dense(data_handler.num_classes, activation="softmax")(output)
 
-        self.model = Model(
-            inputs=[words_input, coordinates, aux_features],
-            outputs=[output]
+        return Model(
+            inputs=[words_input, coordinates, aux_features], outputs=[output]
         )
-        self.model.compile(loss='sparse_categorical_crossentropy',
-                           optimizer='Adam', metrics=['accuracy', f1])
         # self.model.summary()
-        self.data_handler = data_handler
-        self.config = config
 
     def load_weights(self, path):
         """Loads weights from the given model file"""
@@ -215,58 +237,68 @@ class InvoiceNet(InvoiceNetInterface):
         return pred
 
     def evaluate(self):
-        predictions = self.model.predict([self.data_handler.train_data['inputs'],
-                                          self.data_handler.train_data['coordinates']], verbose=True)
+        predictions = self.model.predict(
+            [
+                self.data_handler.train_data["inputs"],
+                self.data_handler.train_data["coordinates"],
+            ],
+            verbose=True,
+        )
         predictions = predictions.argmax(axis=-1)
-        acc = np.sum(predictions == (self.data_handler.train_data['labels']) /
-                     float(len(self.data_handler.train_data['labels'])))
+        acc = np.sum(
+            predictions
+            == (self.data_handler.train_data["labels"])
+            / float(len(self.data_handler.train_data["labels"]))
+        )
         print("\nTest Accuracy: {}".format(acc))
         return predictions
 
     def f1_score(self, predictions):
-        true_labels = self.data_handler.train_data['labels']
+        true_labels = self.data_handler.train_data["labels"]
         f1_scores, macrof1 = get_f1_scores(predictions, true_labels)
         print("\nMacro-Averaged F1: %.4f\n" % macrof1)
         return f1_scores, macrof1
 
 
 class InvoiceNetCloudScan(InvoiceNetInterface):
-
-    def __init__(self, data_handler, config):
+    def create_model(self, data_handler, config):
         num_features = sum(v.shape[1] for v in data_handler.features.values())
-        features = Input(shape=(num_features*5,),
-                         dtype='float32', name='features')
+        features = Input(
+            shape=(num_features * 5,), dtype="float32", name="features"
+        )
 
         if config.num_layers == 2:
-            output = Dense(config.num_hidden, activation='relu')(features)
+            output = Dense(config.num_hidden, activation="relu")(features)
             output = Dropout(0.5)(output)
         else:
             output = features
-        output = Dense(config.num_output,
-                       activation='softmax',
-                       kernel_regularizer=L1L2(l1=0.0, l2=0.1))(output)
-        self.model = Model(inputs=[features], outputs=[output])
-        self.model.compile(optimizer='Adam',
-                           loss='sparse_categorical_crossentropy',
-                           metrics=['accuracy'])
-        print(self.model.summary())
-        self.data_handler = data_handler
-        self.config = config
+        output = Dense(
+            config.num_output,
+            activation="softmax",
+            kernel_regularizer=L1L2(l1=0.0, l2=0.1),
+        )(output)
+        return Model(inputs=[features], outputs=[output])
 
     def prepare_data(self, features, labels):
         features_array = np.concatenate([v for v in features.values()], axis=1)
 
         spatial_features = np.zeros(
-            [features_array.shape[0], features_array.shape[1]*4], dtype=np.float32)
+            [features_array.shape[0], features_array.shape[1] * 4],
+            dtype=np.float32,
+        )
 
         zero_vec = np.zeros(features_array.shape[1], dtype=np.float32)
         for i in range(features_array.shape[0]):
-            vectors = [zero_vec if j == -1 else features_array[j]
-                       # TODO fix this hardcoded reference
-                       for j in self.data_handler.data.at[i, 'closest_ngrams']]
+            vectors = [
+                zero_vec if j == -1 else features_array[j]
+                # TODO fix this hardcoded reference
+                for j in self.data_handler.data.at[i, "closest_ngrams"]
+            ]
             spatial_features[i, :] = np.concatenate(vectors)
 
-        features_array = np.concatenate((features_array, spatial_features), axis=1)
+        features_array = np.concatenate(
+            (features_array, spatial_features), axis=1
+        )
         # TODO add oversampling back
         return features_array, labels
 
