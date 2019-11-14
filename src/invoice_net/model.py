62a6bea5535595a9686a7dac489d4a99beceb8e2
Julius Juodagalvis <julius.juodagalvis@gmail.com>
2019-11-14 16:28:25 +0200

    Simplify CloudScan

--- src/invoice_net/model.py
+++ src/invoice_net/model.py
@@ -1,5 +1,4 @@
 import os
-import pickle
 from typing import Any
 
 import numpy as np
@@ -12,7 +11,6 @@ from tensorflow.keras.layers import (
     Dropout,
     Embedding,
     Input,
-    GlobalMaxPooling1D,
     GRU,
 )
 from tensorflow.keras.regularizers import L1L2
@@ -95,10 +93,13 @@ def f1(y_true, y_pred, num_classes=6):
 
 class InvoiceNetInterface:
     def __init__(self, data_handler, config):
+        print("Initializing model...")
         self.data_handler = data_handler
         self.config = config
+        print("Defining model graph...")
         self.model = self.create_model(data_handler, config)
-        self.model.compile()
+        print("Compiling model...")
+        self.compile_model()
 
     def _create_needed_dirs(self):
         if not os.path.exists(self.config.log_dir):
@@ -156,10 +157,7 @@ class InvoiceNetInterface:
             batch_size=self.config.batch_size,
             verbose=True,
             epochs=self.config.num_epochs,
-            callbacks=[
-                self.tensorboard_callback,
-                self.modelcheckpoints_callback,
-            ],
+            callbacks=[self.tensorboard_callback],
             validation_split=0.125,
             shuffle=self.config.shuffle,
             class_weight=self.get_class_weights(self.data_handler.labels),
@@ -200,18 +198,17 @@ class InvoiceNet(InvoiceNetInterface):
             shape=(data_handler.max_length,), dtype="int32", name="words_input"
         )
         words = Embedding(
-            data_handler.embeddings.shape[0],
+            data_handler.vocab_size,
             data_handler.embeddings.shape[1],
             weights=[data_handler.embeddings],
             trainable=False,
         )(words_input)
-        output = Dropout(0.5)(words)
         output = GRU(
             config.num_hidden,
             dropout=0.5,
             recurrent_dropout=0.5,
             go_backwards=True,
-        )(output)
+        )(words)
         output = concatenate([output, coordinates, aux_features])
         output = Dense(config.num_hidden, activation="relu")(output)
         output = Dense(config.num_hidden, activation="relu")(output)
@@ -228,13 +225,6 @@ class InvoiceNet(InvoiceNetInterface):
         """Loads weights from the given model file"""
         self.model.load_weights(path)
         print("\nSuccessfully loaded weights from {}".format(path))
-
-    def predict(self, tokens, coordinates):
-        """Performs inference on the given tokens and coordinates"""
-        inp, coords = self.data_handler.process_data(tokens, coordinates)
-        pred = self.model.predict([inp, coords], verbose=True)
-        pred = pred.argmax(axis=-1)
-        return pred
 
     def evaluate(self):
         predictions = self.model.predict(
@@ -262,45 +252,36 @@ class InvoiceNet(InvoiceNetInterface):
 
 class InvoiceNetCloudScan(InvoiceNetInterface):
     def create_model(self, data_handler, config):
-        num_features = sum(v.shape[1] for v in data_handler.features.values())
-        features = Input(
-            shape=(num_features * 5,), dtype="float32", name="features"
+        coordinates = Input(
+            shape=(data_handler.train_data["coordinates"].shape[1],),
+            dtype="float32",
+            name="coordinates",
         )
-
-        if config.num_layers == 2:
-            output = Dense(config.num_hidden, activation="relu")(features)
-            output = Dropout(0.5)(output)
-        else:
-            output = features
+        aux_features = Input(
+            shape=(data_handler.train_data["aux_features"].shape[1],),
+            dtype="float32",
+            name="aux_features",
+        )
+        words_input = Input(
+            shape=(data_handler.max_length,), dtype="int32", name="words_input"
+        )
+        words = Embedding(
+            data_handler.vocab_size,
+            data_handler.embeddings.shape[1],
+            weights=[data_handler.embeddings],
+            trainable=False,
+        )(words_input)
+        words = GRU(
+            config.num_hidden, dropout=0, recurrent_dropout=0, go_backwards=True
+        )(words)
+        output = concatenate([words, coordinates, aux_features])
         output = Dense(
-            config.num_output,
-            activation="softmax",
-            kernel_regularizer=L1L2(l1=0.0, l2=0.1),
+            data_handler.num_classes, activation="softmax", name="output"
         )(output)
-        return Model(inputs=[features], outputs=[output])
 
-    def prepare_data(self, features, labels):
-        features_array = np.concatenate([v for v in features.values()], axis=1)
-
-        spatial_features = np.zeros(
-            [features_array.shape[0], features_array.shape[1] * 4],
-            dtype=np.float32,
+        return Model(
+            inputs=[words_input, coordinates, aux_features], outputs=[output]
         )
-
-        zero_vec = np.zeros(features_array.shape[1], dtype=np.float32)
-        for i in range(features_array.shape[0]):
-            vectors = [
-                zero_vec if j == -1 else features_array[j]
-                # TODO fix this hardcoded reference
-                for j in self.data_handler.data.at[i, "closest_ngrams"]
-            ]
-            spatial_features[i, :] = np.concatenate(vectors)
-
-        features_array = np.concatenate(
-            (features_array, spatial_features), axis=1
-        )
-        # TODO add oversampling back
-        return features_array, labels
 
     def load_weights(self, path):
         """Loads weights from the given model file"""
