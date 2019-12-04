4276856a748d440339df6693016614b3fd8403c8
Julius Juodagalvis <julius.juodagalvis@gmail.com>
2019-12-04 04:24:07 +0200

    Run docker as current user

--- tasks.py
+++ tasks.py
@@ -16,9 +16,9 @@ def gpu(context, model_args=""):
     volumes_option_string = " ".join((_volume(v) for v in volumes))
     context.run(
         f"docker run "
+        f"--user $(id -u):$(id -g) "
         f"{volumes_option_string} "
         f"--gpus all "
         f"mutusfa/invoicenet "
         f"python3 {model_args}"
     )
-    print(volumes_option_string)
