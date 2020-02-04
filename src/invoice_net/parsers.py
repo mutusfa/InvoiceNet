fb66098125dd2bf1c0b334a21f72fa28aee95aa2
Julius Juodagalvis <julius.juodagalvis@gmail.com>
2020-02-04 16:36:18 +0200

    Update parsers to work better with data we have

--- src/invoice_net/parsers.py
+++ src/invoice_net/parsers.py
@@ -2,9 +2,26 @@ from datetime import datetime
 import dateutil.parser
 import re
 
+from currencies import MONEY_FORMATS
 import pandas as pd
 from schwifty import IBAN
 import stdnum.lt
+
+# numerical values
+
+ALL_MONEY_FORMATS_SET = set(
+    amount_format
+    for currency in MONEY_FORMATS.values()
+    for amount_format in currency.values()
+)
+
+
+def parses_as_amount(text):
+    amount_pattern = re.compile(r"\d{1,3}(?:[',.]?\d{3})*(?:[,.]\d{2,8})")
+    parsed_str = re.sub(amount_pattern, "{amount}", text)
+    if parsed_str in ALL_MONEY_FORMATS_SET:
+        return True
+    return False
 
 
 def parses_as_number(text):
@@ -13,19 +30,10 @@ def parses_as_number(text):
         return True
     except ValueError:
         pass
-    currencies = [r"\$", "USD", "€", "EUR"]
-    currencies_pattern = "|".join(currencies)
-    amount_pattern = (
-        r"(?:^|\s|"
-        + currencies_pattern
-        + r")\d+\.\d+(?:$|\s|"
-        + currencies_pattern
-        + r")"
-    )
-    try:
-        return bool(re.search(amount_pattern, text)[0])
-    except TypeError:  # no matches
-        return None
+    return parses_as_amount(text)
+
+
+# standard numbers
 
 
 def parses_as_IBAN(text):
@@ -40,9 +48,18 @@ def parses_as_VAT_registration(text):
 
 
 def parses_as_serial_number(text):
-    has_numbers = re.search(r"\d", text)
-    has_letters = re.search(r"[a-zA-Z]", text)
-    return has_numbers and has_letters and text.isalnum()
+    return re.search(r"^[A-Za-z0-9/\-_\\ \t]+$", text)
+
+
+def parses_as_invoice_number(text):
+    return (
+        parses_as_serial_number(text)
+        and not parses_as_VAT_registration(text)
+        and not parses_as_IBAN(text)
+    )
+
+
+# misc
 
 
 def parses_as_full_date(text):
@@ -55,15 +72,18 @@ def parses_as_full_date(text):
         )
     except (ValueError, TypeError, OverflowError):
         return False
-    return min_default.date() == max_default.date()
+    has_two_separators = re.search(r"\d+[^\d]\d+[^\d]\d+", text)
+    return (min_default.date() == max_default.date()) and has_two_separators
 
 
 PARSERS_DATAFRAME = pd.DataFrame(
     [
         (parses_as_full_date, 100, "date",),
+        (parses_as_amount, 900, "amount"),
         (parses_as_number, 1000, "number"),
         (parses_as_IBAN, 200, "iban"),
         (parses_as_VAT_registration, 300, "vat_registration"),
+        (parses_as_invoice_number, 400, "invoice_number"),
         (parses_as_serial_number, 500, "serial_number"),
     ],
     columns=["callable", "priority", "label"],
